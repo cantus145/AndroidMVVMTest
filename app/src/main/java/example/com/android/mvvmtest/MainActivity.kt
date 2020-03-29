eaa84585742900c2ece626f414dd1112a3ca5098
cantus145 <cantus145@hotmail.com>
2020-03-29 23:24:54 +0800

    clean code

--- app/src/main/java/example/com/android/mvvmtest/MainActivity.kt
+++ app/src/main/java/example/com/android/mvvmtest/MainActivity.kt
@@ -1,6 +1,5 @@
 package example.com.android.mvvmtest
 
-import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
